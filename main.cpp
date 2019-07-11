80e7816d14f281856bd819bd96c407d87e838705
Corentin <corentin2kr@gmail.com>
2019-07-11 17:05:36 -0400

    dirent rm + path class rm + basic feature

--- main.cpp
+++ main.cpp
@@ -1,60 +1,54 @@
 
-#include "path.hpp"
 #include <cstdlib>
+#include <exception>
 #include <iostream>
+#include <stdexcept>
 #include <string>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <utility>
 #include <vector>
 
-#include "dirent/include/dirent.h"
-
-void lstest(std::string _dirName)
+bool file_exist(const std::string& _filePath)
 {
-    DIR* dir;
-    struct dirent* ent;
+    struct stat info;
+    return stat(_filePath.c_str(), &info) == 0 && !(info.st_mode & S_IFDIR);
+}
 
-    /* Open directory stream */
-    dir = opendir(_dirName.c_str());
-    if (dir != NULL)
+std::vector<std::string> get_files_paths(int _argc, char const* _argv[])
+{
+    std::vector<std::string> paths;
+    std::string path;
+
+    for (int i = 1; i < _argc; i++)
     {
-        /* Print all files and directories within the directory */
-        while ((ent = readdir(dir)) != NULL)
-        {
-            switch (ent->d_type)
-            {
-                case DT_REG: printf("%s\n", ent->d_name); break;
+        path = _argv[i];
 
-                case DT_DIR: printf("%s/\n", ent->d_name); break;
+        if (!file_exist(path))
+            throw std::runtime_error("error : cannot open \"" + path + "\"");
 
-                case DT_LNK: printf("%s@\n", ent->d_name); break;
-
-                default: printf("%s*\n", ent->d_name);
-            }
-        }
-
-        closedir(dir);
+        paths.push_back(path);
     }
-    else
-    {
-        /* Could not open directory */
-        std::cout << "Cannot open dir : " + _dirName << std::endl;
-    }
+
+    return paths;
 }
 
 // command : meta-cxx *.cpp
 // g++ [METAFILES] -o program(.exe)
-int main(int argc, char const* argv[])
+int main(int _argc, char const* _argv[])
 {
-    std::vector<std::string> files;
-
-    for (int i = 1; i < argc; i++)
+    try
     {
-        std::string file(argv[i]);
-        path p(file);
-        std::cout << p.filename() << std::endl;
-        std::cout << p.directory() << std::endl;
-        std::cout << p.extension() << std::endl;
-        lstest(p.directory());
-        files.push_back(file);
+        const std::vector<std::string>& paths = get_files_paths(_argc, _argv);
+
+        for (size_t i = 0; i < paths.size(); i++)
+        {
+            std::cout << "path : " + paths[i] << std::endl;
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
     }
 
     system("pause");
